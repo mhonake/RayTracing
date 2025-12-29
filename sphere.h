0ac070e1ac08280f8dc56ae77b117f1b8ce6a187
Michael Honaker <mhonaker22@gmail.com>
2025-12-29 09:23:39 -0500

    Made it to 6.5

---
+++ sphere.h
@@ -0,0 +1,55 @@
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+
+class sphere : public hittable
+{
+public:
+    sphere(const point3& center, const double& radius):
+        center(center),
+        radius(std::fmax(0,radius))
+    {}
+
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec)
+    {
+        vec3 oc = center - r.origin();
+        double a = r.direction().length_squared();
+        double h = dot(r.direction(), oc);
+        double c = oc.length_squared() - radius*radius;
+        double discriminant = h*h - a*c;
+
+        if (discriminant < 0)
+        {
+            return false;
+        }
+        
+        double sqrtd = std::sqrt(discriminant);
+
+        double root = (h - sqrtd) / a;
+
+        // Find the nearest root that lies in the acceptable range.
+        if (root <= ray_tmin || root >= ray_tmax)
+        {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || root >= ray_tmax)
+            {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+
+private:
+    point3 center;
+    double radius;
+};
+
+#endif
